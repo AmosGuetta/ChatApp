3ff220c891acdb4dcee23c871fc48e9c6925bcb0
Amos Guetta <amos.guetta.1990@gmail.com>
2018-09-12 18:14:03 +0300

    Retrieve messages from firebase function

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -18,10 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        FirebaseApp.configure()
-        let myDatabase = Database.database().reference()
-        myDatabase.setValue("My First Data")
-        
+        FirebaseApp.configure()        
         return true
     }
 
